054ff64d4aa5109f0a1ef7b6d400663a816faf12
anch0vy <anch0vy.korea@gmail.com>
2026-01-04 19:18:55 +0900

    푸터 작업

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`antialiased`}
+        className="antialiased h-screen bg-background text-foreground overflow-auto flex flex-col max-w-[1280px] mx-auto"
       >
-        {children}
+        <main className="pb-24 mx-auto flex-1 w-full">
+          {children}
+        </main>
+        <footer className="border-t-2 border-foreground py-2 text-[12px] w-full">
+          <div className="flex justify-end w-full mx-auto">
+            <p>만돌린의 리카이시스의 사랑으로 만들어짐.</p>
+          </div>
+        </footer>
       </body>
     </html>
   );
