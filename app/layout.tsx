33fd5c50f7233142837f676c8807bed74760b559
anch0vy <anch0vy.korea@gmail.com>
2026-01-04 19:29:49 +0900

    간단한 헤더 작성

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Image from "next/image";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,10 +17,14 @@ export default function RootLayout({
       <body
         className="antialiased h-screen bg-background text-foreground overflow-auto flex flex-col max-w-[1280px] mx-auto"
       >
+        <header className="border-b border-foreground py-2 text-[24px] w-full flex items-center gap-2">
+          <Image src="/symbol_C.png" alt="logo" width={32} height={32} />
+          <p className="font-bold" style={{transform: "translateY(2px)"}}>마비노기 경매장</p>
+        </header>
         <main className="pb-24 mx-auto flex-1 w-full">
           {children}
         </main>
-        <footer className="border-t-2 border-foreground py-2 text-[12px] w-full">
+        <footer className="border-t border-foreground py-2 text-[12px] w-full">
           <div className="flex justify-end w-full mx-auto">
             <p>만돌린의 리카이시스의 사랑으로 만들어짐.</p>
           </div>
